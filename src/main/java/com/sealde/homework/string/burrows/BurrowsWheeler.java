8bbe6521bab2f1f3a607af04d9448a2c69870ac1
lizhide <lizhide@corp.netease.com>
2020-11-03 01:19:16 +0800

    Mod

--- src/main/java/com/sealde/homework/string/burrows/BurrowsWheeler.java
+++ src/main/java/com/sealde/homework/string/burrows/BurrowsWheeler.java
@@ -3,6 +3,8 @@ package com.sealde.homework.string.burrows;
 import edu.princeton.cs.algs4.BinaryStdIn;
 import edu.princeton.cs.algs4.BinaryStdOut;
 
+import java.util.Arrays;
+
 public class BurrowsWheeler {
     // apply Burrows-Wheeler transform,
     // reading from standard input and writing to standard output
@@ -28,15 +30,34 @@ public class BurrowsWheeler {
     // apply Burrows-Wheeler inverse transform,
     // reading from standard input and writing to standard output
     public static void inverseTransform() {
-
+        int first = BinaryStdIn.readInt();
+        String s = BinaryStdIn.readString();
+        CircularSuffixArray csa = new CircularSuffixArray(s);
+        int length = csa.length();
+        int[] next = new int[length];
+        char tmp = s.charAt(csa.index(0));
+        // 初始化 next
+        for (int i = 0, count = 0; i < length; i++) {
+            next[i] = csa.index(i);
+            char c = s.charAt(next[i]);
+            if (tmp != c || i == length-1) {
+                tmp = c;
+                Arrays.sort(next, i-count, i == length-1 ? i+1 : i);
+                count = 1;
+            } else count++;
+        }
+        // 输出
+        for (int i = 0, j = first; i < length; i++, j = next[j])
+            BinaryStdOut.write(s.charAt(next[j]));
+        BinaryStdOut.close();
     }
 
     // if args[0] is "-", apply Burrows-Wheeler transform
     // if args[0] is "+", apply Burrows-Wheeler inverse transform
     public static void main(String[] args) {
         String op = args[0];
-        if ("-".equals(op)) {
-            transform();
-        }
+        if ("-".equals(op)) transform();
+        else if ("+".equals(op)) inverseTransform();
+        else throw new IllegalArgumentException();
     }
 }
